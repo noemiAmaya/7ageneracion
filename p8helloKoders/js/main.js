fc6e63b7eded928b76ba0efaac925352ec915093
noemiAmaya <jazzamaya13@gmail.com>
2020-03-04 12:36:09 -0600

    practicing console-log

---
+++ p8helloKoders/js/main.js
@@ -0,0 +1,11 @@
+var x = 10
+var y = 15
+
+var plus = x+y 
+console.log (plus)
+var minus = x-y 
+console.log (minus)
+var multiply= x*y 
+console.log (multiply)
+var divide= x/y 
+console.log (divide)
